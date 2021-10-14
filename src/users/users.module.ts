c4ce486cb308a1f4c4e71fc68d07a67d2e2a06e1
Caio Dariva <cgmalavazidariva@gmail.com>
2021-10-13 22:31:41 -0300

    create roles, service, userdto and route createadm

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
+import { UsersService } from './users.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature(UserRepository)],
+  providers: [UsersService],
+})
 export class UsersModule {}
