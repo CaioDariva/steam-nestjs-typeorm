db3e5d50241798bd5fc3eb793e80f45067db9a46
Caio Dariva <cgmalavazidariva@gmail.com>
2021-10-14 20:58:47 -0300

    validations in dto

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,10 +1,19 @@
 import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-  @IsNotEmpty({ message: 'Informe um endereço de e-mail' })
-  @IsEmail({}, { message: 'Utilize um e-mail válido' })
+  @IsNotEmpty({ message: 'Informe um endereço de e-mail.' })
+  @IsEmail({}, { message: 'Utilize um e-mail válido.' })
+  @MaxLength(200, { message: 'O endereço de e-mail é muito grande.' })
   email: string;
+  @IsNotEmpty({ message: 'Informe o nome do usuário.' })
+  @MaxLength(200, { message: 'O nome é muito grande.' })
   name: string;
+  @IsNotEmpty({ message: 'Informe uma senha.' })
+  @MinLength(6, { message: 'A senha deve ter pelo menos 6 caracteres.' })
   password: string;
+  @IsNotEmpty({ message: 'Informe a confirmação de senha.' })
+  @MinLength(6, {
+    message: 'A confirmação de senha deve ter pelo menos 6 caracteres.',
+  })
   passwordConfirmation: string;
 }
