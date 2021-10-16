73a6df6f7fe062701964c3cf5c3fdd623252d5af
Caio Dariva <cgmalavazidariva@gmail.com>
2021-10-15 22:00:01 -0300

    imports strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,5 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from 'src/users/users.repository';
